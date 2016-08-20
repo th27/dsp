9174609aaca91aaf22222706b7ffaa36188cff59
th27 <taichi27@gmail.com>
2016-08-20 13:11:48 +1000

    q5_datetime.py completed

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,30 @@
 # Hint:  use Google to find python function
 
+from datetime import date
+
+def diff_dates(date_start, date_stop):
+	return abs(date_stop - date_start).days
+
 ####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+# date_start = '01-02-2013'  
+# date_stop = '07-28-2015'   
+
+date_start = date(2013,1,2)
+date_stop = date(2015,7,28)
+print "%d days difference" % diff_dates(date_start,date_stop)   
 
 ####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+# date_start = '12312013'  
+# date_stop = '05282015'  
+
+date_start = date(2013,12,31)
+date_stop = date(2015,5,28)
+print "%d days difference" % diff_dates(date_start,date_stop)   
 
 ####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+# date_start = '15-Jan-1994'  
+# date_stop = '14-Jul-2015'  
+
+date_start = date(1994,1,15)
+date_stop = date(2015,7,14)
+print "%d days difference" % diff_dates(date_start,date_stop)   
